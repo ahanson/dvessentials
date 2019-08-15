3f136203ce6ed26650c13419482cd35660e3c775
Alex Hanson <ahanson@runbox.com>
2019-08-15 13:46:20 -0500

    adds apache recipe to recipes/default.rb

---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: devessentials
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+# This is my first attempt at using Nano.
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
